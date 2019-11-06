58fa4706512f20c0b761ce7f07f6e732488610bd
joytou <1540294142@qq.com>
2019-11-06 20:45:31 +0800

    Upload the project codes, with modified the README.md word.

---
+++ js/ie8-responsive-file-warning.js
@@ -0,0 +1,3 @@
+if (window.location.protocol == 'file:') {
+  window.alert('ERROR: Bootstrap\'s responsive CSS is disabled!\nSee getbootstrap.com/getting-started/#respond-file-proto for details.')
+}
